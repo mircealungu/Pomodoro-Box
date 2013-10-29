45eab8819d8da9987422e40972c97030e2768275
Mircea Lungu <mircea.lungu@gmail.com>
2013-10-29 16:42:28 +0100

    added the test project

---
+++ android/PomodoroBox-Test/src/lu/mir/android/pomodorobox/test/TestPomodoro.java
@@ -0,0 +1,19 @@
+package lu.mir.android.pomodorobox.test;
+
+import junit.framework.TestCase;
+
+public class TestPomodoro extends TestCase {
+
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+	
+	public void testTrue() {
+		assertEquals(1, 1);
+	}
+
+}
